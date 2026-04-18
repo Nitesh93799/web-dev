740ded53907dd856440a667e8fa0c13f5a12d8d7
Nitesh Shahi <n.shahi.1558@westcliff.edu>
2026-04-18 19:19:43 +0545

    project of wed development

---
+++ personalproject/script.js
@@ -0,0 +1,39 @@
+// NEPAL TIME
+function updateNepalClock() {
+    const options = {
+        timeZone: "Asia/Kathmandu",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    };
+
+    const time = new Intl.DateTimeFormat([], options).format(new Date());
+    document.getElementById("nepal-clock").innerText = time;
+}
+
+// USER TIME
+function updateUserClock() {
+    const now = new Date();
+    document.getElementById("user-clock").innerText =
+        now.toLocaleTimeString();
+}
+
+// AUTO THEME
+function updateTheme() {
+    const hour = new Date().getHours();
+
+    if (hour >= 6 && hour < 18) {
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+    } else {
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+    }
+}
+
+// RUN EVERY SECOND
+setInterval(() => {
+    updateNepalClock();
+    updateUserClock();
+    updateTheme();
+}, 1000);
